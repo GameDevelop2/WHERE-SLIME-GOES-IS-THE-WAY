63846fefa346a0fd718189808dd65c77ded36af6
survedog <survedog@gmail.com>
2023-05-26 19:30:34 +0900

    [FEAT]스프링 스크립트 및 프리팹

---
+++ Assets/Scripts/Items/JumpSpring.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class JumpSpring : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed = 10f;
+    private static Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        if (!playerRigidbody)
+            playerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed);
+    }
+}
