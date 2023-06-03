0ef8ca984356821966f739c1b609098a707b64e5
survedog <survedog@gmail.com>
2023-06-03 22:58:59 +0900

    [FEAT] 포탈 아이템 기능 구현 - Portal 스크립트 및 아이템 프리팹 추가 - 플레이어 Rigidbody LinearDrag 설정해   최대 낙하 속도 제한 - Stage2의 스프링 점프 높이 조정   - 캐릭터의 질량이 달라져 점프 높이가 바뀌었었음

---
+++ Assets/Scripts/Items/Portal.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public bool isBluePortal;
+    [HideInInspector] public bool isTeleportUnabled;
+    public static Portal bluePortal = null, orangePortal = null;
+
+
+    void OnEnable()
+    {
+        isTeleportUnabled = false;
+        if (isBluePortal)
+        {
+            if (bluePortal)
+                Destroy(bluePortal.gameObject);
+            bluePortal = this;
+        }
+        else
+        {
+            if (orangePortal)
+                Destroy(orangePortal.gameObject);
+            orangePortal = this;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (isBluePortal)
+            bluePortal = null;
+        else
+            orangePortal = null;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isTeleportUnabled)
+            return;
+
+        if (isBluePortal)
+        {
+            collision.transform.position = orangePortal.transform.position;
+            orangePortal.isTeleportUnabled = true;
+        }
+        else
+        {
+            collision.transform.position = bluePortal.transform.position;
+            bluePortal.isTeleportUnabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTeleportUnabled = false;
+    }
+}
