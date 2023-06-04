e5ea9bfd1c6cad357e843e57564753d77ffeb199
survedog <survedog@gmail.com>
2023-06-04 22:42:23 +0900

    [FEAT&FIX] 락 스포너, LO포탈 등 프리팹 방식으로 개선 - RespawnObstacle 스크립트: 플레이어 접촉 시 리스폰시킴 - ObjectSpawner 스크립트: 오브젝트를 소환하기 위해 사용   - 한 번만 소환할 지, 반복해서 소환할 지 선택 가능. - LO_FallingRock, LO_FallingRockSpawner 프리팹 추가 - LO_Portal_Orange, LO_Portal_Blue 프리팹 추가   - 기존 LO_EnterPortal, LO_ExitPortal은     충돌 방지를 위해 남겨둠. - Stage 2 수정사항   - 락 스포너, 포탈 모두 프리팹으로 대체   - YoonLevelManager 오브젝트 & 스크립트 삭제

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [Header("Common Setting")]
+    [SerializeField] private GameObject objectToSpawn;
+    [Tooltip("스포너의 Awake 시점 부터 오브젝트 최초 스폰 까지의 시간")]
+    [SerializeField] private float initialDelayBeforeSpawn = 0f;
+    [Tooltip("오브젝트가 스폰될 상대적 위치")]
+    [SerializeField] private Vector3 spawnPositionOffset;
+    [Tooltip("오브젝트 스폰 후 스스로를 Destroy 할 지 여부")]
+    [SerializeField] private bool destroySelfAfterObjectSpawn;
+
+    [Space(10f)]
+    [Header("Repeat Setting")]
+    [Tooltip("오브젝트를 반복적으로 스폰할 지 여부")]
+    [SerializeField] private bool repeatSpawning = false;
+    [SerializeField] private float spawnPeriod = 1f;
+    private float currentSpawnCoolTime = 1f;
+
+    void Awake()
+    {
+        currentSpawnCoolTime = initialDelayBeforeSpawn;
+    }
+
+    void Update()
+    {
+        currentSpawnCoolTime -= Time.deltaTime;
+        if (currentSpawnCoolTime <= 0f)
+        {
+            Instantiate<GameObject>(objectToSpawn, transform.position + spawnPositionOffset, objectToSpawn.transform.rotation);
+
+            if (destroySelfAfterObjectSpawn)
+                Destroy(gameObject);
+            else if (repeatSpawning)
+                currentSpawnCoolTime = spawnPeriod;            
+        }
+    }
+}
---
+++ Assets/Scripts/RespawnObstacle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RespawnObstacle : MonoBehaviour
+{
+    [Tooltip("충돌 발생 시 스스로를 Destroy 할 지 여부")]
+    [SerializeField] private bool destroyOnCollision = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) // 플레이어에 닿은 경우
+            collision.gameObject.GetComponent<PlayerBehavior>().Respawn();
+
+        if (collision.CompareTag("Portal")) // 포탈에 닿은 경우엔 스스로를 삭제하지 않는다. (단, MapStaticObject 레이어로 설정 시엔 설정 상 포탈과 충돌 판정이 발생하지 않음)
+            return;
+        else if (destroyOnCollision)
+            Destroy(gameObject);
+    }
+}
