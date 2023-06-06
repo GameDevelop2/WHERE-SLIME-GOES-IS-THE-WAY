b8dd258c2808cfc98b40020deb5378dc0a6e54c7
survedog <survedog@gmail.com>
2023-06-06 11:01:30 +0900

    [FEAT]스프링 스프라이트 및 애니메이션 적용

--- Assets/Scripts/Items/JumpSpring.cs
+++ Assets/Scripts/Items/JumpSpring.cs
@@ -4,16 +4,21 @@ public class JumpSpring : MonoBehaviour
 {
     [SerializeField] private float jumpSpeed = 10f;
     private Rigidbody2D playerRigidbody;
+    Animator animator;
 
     private void Awake()
     {
         if (!playerRigidbody)
             playerRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
+        {
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed);
+            animator.SetTrigger("Bounce");
+        }
     }
 }
